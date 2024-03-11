39abfdea41fef0be7cfd783bee3b0d1388acb347
MD SIFAT ALI <sifatali520@gmail.com>
2024-03-11 19:33:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# print("Hello, World")
+print("Hello, World")
 
-# if __name__ == '__main__':
-#     a = int(input())
-#     b = int(input())
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# a = 4
-# b = 3
-# print(a//b)
-# print(a/b)
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
+a = 4
+b = 3
+print(a//b)
+print(a/b)
 
-# n = int(input())
-# for i in range (n):
-#     print(i ** 2)
+n = int(input())
+for i in range (n):
+    print(i ** 2)
 
-# n = 5
-# for i  in range (1, n+1):
-#         print(i, end='')
+n = 5
+for i  in range (1, n+1):
+        print(i, end='')
 ab = 5
