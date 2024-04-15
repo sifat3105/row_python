e3ff421a7ce3d6b1f461a580aa7e66d9860a62cc
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-15 08:09:29 +0600

    Update name_pass.py

--- name_pass.py
+++ name_pass.py
@@ -2,11 +2,14 @@ import random
 
 target= input("enter pass ; ")
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
 while True:
+    number = ''.join(str(random.randint(0, 9)) for _ in range(5))
+    letter = ''.join(random.choice(letters) for _ in range(1))
     matched = False
-    for _ in range(7):
-        number = ''.join(str(random.randint(0, 9)) for _ in range(3))
-        letter = ''.join(random.choice(letters) for _ in range(5))
+    for _ in range(1):
+        
         password = letter + number
         print(password)
         if target == password:
