9c9a1fc3e8a80f38e2fab15599d0640943ad05e9
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-15 06:27:35 +0600

    Update password.py

--- password.py
+++ password.py
@@ -1,11 +1,10 @@
 import random
 
 target = 1021
-num = ''.join(str(random.randint(1, 9)) for _ in range(4))
 for _ in range(10000000):
     matched = False
     for _ in range(3):
-        number = int(num)
+        number = int(''.join(str(random.randint(1, 9)) for _ in range(4)))
         if target == number:
             matched = True
             print(number)
